47093a6b59e0aad641a284c129d792b42a4cc68b
pedelgado <pedro.delgado.alc@gmail.com>
2021-02-25 23:19:23 +0100

    New Player controller script

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,241 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Settings/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""e6066dba-a102-4dde-9905-417982fe57a9"",
+            ""actions"": [
+                {
+                    ""name"": ""Camera"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""443c7e8a-c457-4a8c-83cd-404441d109f2"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""2f26bdb1-66ba-4594-b558-f1205376838c"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""a0ef2cbd-0a8c-4751-b9fe-0a351cde124f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""6a443d43-f22f-4cc2-9230-e79fe4726817"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""01b1d537-e64e-47dc-9333-9f5b4454742e"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone(min=0.15)"",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""94425131-3d1b-46fb-904d-fb09d31a23b3"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""eed51f67-535e-40eb-a87f-fc9068587963"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c73be77d-d150-49bb-9c49-acfcff6f6ba7"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5f31e7dc-a80c-436f-ae1d-3654850f32a7"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1a2f5bb4-a173-450a-8d62-f28057ff99eb"",
+                    ""path"": ""<Gamepad>/rightShoulder"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Camera = m_Gameplay.FindAction("Camera", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
+        m_Gameplay_Run = m_Gameplay.FindAction("Run", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Camera;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_Jump;
+    private readonly InputAction m_Gameplay_Run;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Camera => m_Wrapper.m_Gameplay_Camera;
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
+        public InputAction @Run => m_Wrapper.m_Gameplay_Run;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Camera.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnCamera;
+                @Camera.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnCamera;
+                @Camera.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnCamera;
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Run.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRun;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Camera.started += instance.OnCamera;
+                @Camera.performed += instance.OnCamera;
+                @Camera.canceled += instance.OnCamera;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnCamera(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+    PlayerControls controls;
+
+    public Transform cam;
+
+    public float walkSpeed = 1.7f;
+    public float runSpeed = 5f;
+    public float turnSmoothTime = 0.1f;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float gravity = -18f;
+    public float groundDistance = 0.01f;
+    public float jumpHeight = 1f;
+    public float neutralVerticalVelocity = 0f;
+
+    private float vertical;
+    private float horizontal;
+    private float speed;
+    private float turnSmoothVelocity;
+    private float targetAngle;
+    private float angle;
+
+    private Vector2 inputDirection;
+    private Vector3 direction;
+    private Vector3 moveDirection;
+    private Vector3 moveDirectionNormalized;
+    private Vector3 velocity; // Vertical velocity
+
+    // Character status
+    private bool running;
+
+    // Character event actions
+    private bool toJump;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        // Control events binding
+        controls.Gameplay.Jump.performed += ctx => toJump = ctx.ReadValueAsButton();
+        controls.Gameplay.Run.performed += ctx => running = ctx.ReadValueAsButton();
+        controls.Gameplay.Move.performed += ctx => inputDirection = ctx.ReadValue<Vector2>();
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (toJump)
+        {
+            jump();
+            toJump = false;
+        }
+
+        move();
+
+        applyGravity();
+        setAnimation();
+    }
+
+    // Applies horizontal movement.
+    void move()
+    {
+        // direction vector will determine if there is movement
+        direction = new Vector3(inputDirection.x, 0f, inputDirection.y);
+
+        if (running) speed = runSpeed;
+        else speed = walkSpeed;
+
+        if (isMoving())
+        {
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            moveDirectionNormalized = moveDirection.normalized * speed;
+
+            controller.Move(moveDirectionNormalized * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+    }
+
+    // Perform a jump
+    void jump()
+    {
+        if (!isJumping() && isGrounded()) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            controller.Move(velocity * Time.deltaTime);
+            animator.SetTrigger("jump");
+        }
+    }
+
+    // Applies Gravity
+    void applyGravity() {
+        // Reset falling velocity when touch the ground.
+        if (isGrounded() && velocity.y > 0)
+            velocity.y = neutralVerticalVelocity;
+
+        // Calculates and applies gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    // Return if character is moving.
+    bool isMoving() {
+        return (direction.magnitude >= 0.1f);
+    }
+
+    // Return if character is in the ground.
+    bool isGrounded() {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+
+    // Return if character is falling.
+    bool isFalling() {
+        return (isGrounded() && velocity.y < neutralVerticalVelocity);
+    }
+
+    // Return if the character is jumping
+    bool isJumping() {
+        return (!isGrounded() && velocity.y > neutralVerticalVelocity);
+    }
+
+    // Set animation variables.
+    void setAnimation()
+    {
+        animator.SetBool("walking", isMoving());
+        animator.SetBool("running", running);
+    }
+}
