100050cb91fbdbf513460389ebbf533502d7a8e4
johan <jccasqui@gmail.com>
2021-02-26 20:01:09 +0100

    pruebas 1

---
+++ Assets/Scripts/Camilo/Proto_enemigo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Proto_enemigo : MonoBehaviour
+{
+    //Referencia al juagdor.
+    public GameObject yo;
+    //parametro de animación para que el enemigo localice al jugador.
+    bool detectado = false;
+    //Velocidad de rotación.
+    float velRot = 12.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yo = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 posicion = transform.position;
+        Vector3 posicionYo = yo.transform.position;
+        float distancia = Vector3.Distance (posicionYo, posicion);
+        if (detectado)
+        {
+            float toWayPoint = Vector3.Distance(posicionYo,transform.position);
+        }
+        
+    }
+    void seguir(GameObject target)
+    {
+        Vector3 posicion = transform.position;
+        Vector3 posicionTarget = target.transform.position;
+        Vector3 direccion = posicionTarget - posicion;
+        transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(direccion, Vector3.up), velRot * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Camilo/control_personaje_proto.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control_personaje_proto : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    public Animator anim;
+    bool izquierda = false;
+    bool derecha = false;
+    bool caminar = false;
+    bool atras = false;
+    
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();   
+        controller.center = new Vector3(0,1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if(groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3 (Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if(move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        //cambia la posicion de altura del jugador.
+        if(Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+        
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("caminar",true);
+        }
+        else
+        {
+            anim.SetBool("caminar", false);
+        }
+
+       
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetBool("atras",true);
+        }
+        else
+        {
+            anim.SetBool("atras", false);
+        }
+
+     
+    }
+   
+}
